343e5eb159703669fb1b9ab794864ef739eb8934
Aimar Ibarra <aimar_ibarra@proton.me>
2024-10-23 12:21:54 +0200

    conexion sql hecha

--- src/main/java/es/rolfan/app/Main.java
+++ src/main/java/es/rolfan/app/Main.java
@@ -1,9 +1,29 @@
-package resolucion;
+package es.rolfan.app;
 
-import menu.Entry;
-import menu.Menu;
+import es.rolfan.menu.Entry;
+import es.rolfan.menu.Menu;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main extends Menu {
+    private static final Scanner sc = new Scanner(System.in);
+
+    private final Connection conn;
+
+    public Main(String host, String port, String user, String password) throws SQLException {
+        conn = DriverManager.getConnection(host + ":" + port, user, password);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main("localhost", "3306", "admin", "1234").runMenuForever();
+    }
+
     @Entry(position = 0, description = "Salir del programa")
     private void salir() {
         System.exit(0);
@@ -11,10 +31,31 @@ public class Main extends Menu {
 
     @Entry(position = 1, description = "Crear BBDD MySQL")
     private void crearBBDDMySQL() {
-        System.out.println(getClass().getResource("resources/olimpiadas.db.sql"));
-    }
+        var url = getClass().getResource("olimpiadas.db.sql");
+        if (url == null) {
+            System.err.println("Falta el archivo sql");
+            return;
+        }
 
-    public static void main(String[] args) throws Exception {
-        new Main().runMenuForever();
+        System.out.println("Introduce el nombre del archivo");
+        var csv = new File(sc.next());
+        if (!csv.exists() || !csv.isFile()) {
+            System.err.println("El archivo csv no existe");
+            return;
+        }
+
+        try (var st = conn.createStatement()) {
+            st.executeUpdate(Files.readString(new File(url.getFile()).toPath()));
+            // TODO: Cargar archivo csv
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            return;
+        } catch (SQLException e) {
+            System.err.println("Ha ocurrido algún error en la carga");
+            return;
+        }
+
+        System.out.println("La carga de la información se ha realizado correctamente");
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module es.rolfan.app {
+    requires mysql.connector.j;
+    requires java.sql;
+    exports es.rolfan.app;
+}
